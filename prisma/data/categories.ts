cd777f73d4781c79b386cb4e831d2442d2493da5
gitrenzo <renzoelm25@gmail.com>
2023-07-24 22:00:54 -0400

    Project updated

---
+++ prisma/data/categories.ts
@@ -0,0 +1,31 @@
+
+const categories = [
+    {
+        icon: "coffee",
+        name: "Coffee"
+      },
+      {
+        icon: "burger",
+        name: "Burgers"
+      },
+      {
+        icon: "pizza",
+        name: "Pizzas"
+      },
+      {
+        icon: "donut",
+        name: "Donut's"
+      },
+      {
+        icon: "cake",
+        name: "Cakes"
+      },
+      {
+        icon: "cookie",
+        name: "Cookies"
+      }
+]
+
+export {
+    categories 
+}
